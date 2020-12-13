95e8a72c9b03143643bdd1026e5e1b6e9cf220cc
nyralith <walkarel123@hotmail.com.ar>
2020-12-13 00:31:14 -0300

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,46 +3,63 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+
+import './css/footer.css'
+
+
+const fbIcon = <FontAwesomeIcon icon={faFacebookSquare} size="lg" />;
+const igIcon = <FontAwesomeIcon icon={faInstagram} size="lg" />;
+const wpIcon = <FontAwesomeIcon icon={faWhatsapp} size="lg" />;
 
 const Footer = () => {
   return (
     <Container
       fluid
-      className="bg-warning d-flex flex-wrap justify-content-around text-center mt-5">
+      className="d-flex flex-wrap justify-content-around text-center mt-5 footerbg">
       <Row>
         <Col>
-          <Image src="/logo500.png" fluid style={{maxHeight:"250px"}}/>
+          <Image src="/logo500.png" fluid style={{ maxHeight: "250px" }} />
         </Col>
         <Col>
-          <h6>Links Columna 1</h6>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-            provident, veritatis minima nesciunt necessitatibus tenetur quaerat
-            dicta maxime dignissimos, obcaecati saepe eius? Dolorem vitae
-            accusamus aut maxime sunt alias inventore.
-          </div>
+          <h6>Links Útiles</h6>
+          <Col>
+            Contacto
+              </Col>
+          <Col>
+            FAQ
+              </Col>
+          <Col>
+            Privacy Policy
+              </Col>
+              <Col>
+              Refund Policy
+              </Col>
+              <Col>
+              Terminos de servicio
+              </Col>
+
         </Col>
         <Col>
-          <h6>Links Columna 2</h6>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-            totam corporis sequi minima modi facere commodi optio tempora
-            facilis incidunt, illo sapiente fuga praesentium? Obcaecati corporis
-            veritatis libero iure dignissimos.
-          </div>
+          <h6>Contacto</h6>
+          <a href="https://www.facebook.com">{fbIcon}</a>
+          <a href="https://www.instagram.com/">{igIcon}</a>
+          <a href="https://www.whatsapp.com/?lang=es">{wpIcon}</a>  
         </Col>
         <Col>
-          <h6>Redes sociales + info contacto + QR Data Fiscal</h6>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-            totam corporis sequi minima modi facere commodi optio tempora
-            facilis incidunt, illo sapiente fuga praesentium? Obcaecati corporis
-            veritatis libero iure dignissimos.
-          </div>
+        <a href="http://qr.afip.gob.ar/?qr=YXKxvKnIS73rllqkgSWURw,," target="_blank" title="AFIP">
+            <Image src="https://www.carrefour.com.ar/media/wysiwyg/footer/afip.png" alt="AFIP"/>
+        </a>
         </Col>
       </Row>
       <Row>
+        <Col>
         <h6> &copy;Copyright 2020</h6>
+        </Col>
       </Row>
     </Container>
   );
