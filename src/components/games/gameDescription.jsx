13f57da1fec2df57079c411b1dda55614b19d065
Benjamin Lavena <benja@zmaster.com.ar>
2020-12-14 22:26:43 -0300

    Creo el checkout (faltan estilos al form de tarjeta y link de MP)

--- src/components/games/gameDescription.jsx
+++ src/components/games/gameDescription.jsx
@@ -19,26 +19,33 @@ const GameDescription = ({ games, addItem }) => {
     price: 0,
   }; //filtro los juegos por el id, como devuelve un array, elijo el primer (y único) item
 
-  return ( 
-    <div className="container gamedescrip " >
+  return (
+    <div className="container gamedescrip ">
       <Row>
-        <Col  sm={5}>
-        <img className="gameimg flex-grow-1"
-          variant="top"
-          src={`/images/${game.images.poster_image_url}`}
-          alt={game.name}
-        />
-      
-      </Col>
-      <Col sm={7}>
-      <h3>{game.name}</h3>
-      <p>{game.description}</p>
-      <p><Price game={game} /></p>
-      <Button variant="primary" style={{ cursor: "pointer" }} onClick={(e) => addItem(e, game)}> {cartIcon} Agregar al carrito</Button>{' '}
-      
-      </Col>
-    
-    </Row>
+        <Col sm={5} className="overflow-hidden">
+          <img
+            className="gameimg flex-grow-1"
+            variant="top"
+            src={`/images/${game.images.poster_image_url}`}
+            alt={game.name}
+          />
+        </Col>
+        <Col sm={7}>
+          <h3>{game.name}</h3>
+          <p>{game.description}</p>
+          <p>
+            <Price game={game} />
+          </p>
+          <Button
+            variant="primary"
+            style={{ cursor: "pointer" }}
+            onClick={(e) => addItem(e, game)}
+          >
+            {" "}
+            {cartIcon} Agregar al carrito
+          </Button>{" "}
+        </Col>
+      </Row>
     </div>
   );
 };
